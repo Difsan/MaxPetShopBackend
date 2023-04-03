4c8ec673fbca5c3f001cbbf40373038e89f46ea2
Difsan <difsanchezru@gmail.com>
2023-04-03 10:52:53 -0500

    CartUseCases were added.

---
+++ domain/usecase/src/main/java/co/com/maxpetshop/usecase/updatecart/UpdateCartUseCase.java
@@ -0,0 +1,19 @@
+package co.com.maxpetshop.usecase.updatecart;
+
+import co.com.maxpetshop.model.cart.Cart;
+import co.com.maxpetshop.model.cart.gateways.CartRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
+
+@RequiredArgsConstructor
+public class UpdateCartUseCase implements BiFunction<String, Cart, Mono<Cart>> {
+
+    private final CartRepository cartRepository;
+
+    @Override
+    public Mono<Cart> apply(String cartId, Cart cart) {
+        return cartRepository.updateCart(cartId, cart);
+    }
+}
